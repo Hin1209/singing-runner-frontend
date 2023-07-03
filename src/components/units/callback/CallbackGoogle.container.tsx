248f54213eef3bd3495438a030f423d9b354f9e5
KoziKim <kory0711@gmail.com>
2023-07-04 03:24:06 +0900

    temp2

--- src/components/units/callback/CallbackGoogle.container.tsx
+++ src/components/units/callback/CallbackGoogle.container.tsx
@@ -2,7 +2,11 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
-import { accessTokenState, userIdState } from "../../../commons/store";
+import {
+  accessTokenState,
+  googleUserResponseState,
+  userIdState,
+} from "../../../commons/store";
 import { gql, useMutation } from "@apollo/client";
 
 export const LOGIN_USER_WITH_GOOGLE = gql`
@@ -21,9 +25,7 @@ export default function CallbackGoogle() {
   const [loginWithGoogle] = useMutation(LOGIN_USER_WITH_GOOGLE);
   const [, setAccessToken] = useRecoilState(accessTokenState);
   const [, setUserId] = useRecoilState(userIdState);
-  // const [, setGoogleUserResponse] = useRecoilState(
-  //   GoogleUserResponseState
-  // );
+  const [, setGoogleUserResponse] = useRecoilState(googleUserResponseState);
 
   useEffect(() => {
     const handleGoogleLogin = async () => {
@@ -31,41 +33,44 @@ export default function CallbackGoogle() {
       const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
       const clientSecret = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
 
+      // 엑세스 토큰 및 사용자 정보 가져오기
+      const response = await axios.post<{
+        access_token: string;
+        id_token: string;
+      }>("https://oauth2.googleapis.com/token", null, {
+        params: {
+          client_id: clientId,
+          clientSecret,
+          redirect_uri: "http://localhost:3001/callback/google",
+          scope:
+            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+          code,
+          grant_type: "authorization_code",
+        },
+      });
+      const { data } = response;
+
+      // 엑세스 토큰에서 사용자 ID 가져오기
+      const {
+        data: { sub, email }, // 변수 sub 및 email 추가
+      } = await axios.get<{
+        sub: string;
+        email: string; // email 속성 추가
+      }>("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`,
+        },
+      });
+
+      const googleUserResponse = {
+        googleId: sub, // 구글 사용자 ID
+        google_account: {
+          // 구글 사용자 이메일
+          email,
+        },
+      };
+
       try {
-        // 엑세스 토큰 및 사용자 정보 가져오기
-        const response = await axios.post<{
-          access_token: string;
-          id_token: string;
-        }>("https://oauth2.googleapis.com/token", null, {
-          params: {
-            client_id: clientId,
-            clientSecret,
-            redirect_uri: "http://localhost:3001/callback/google",
-            scope:
-              "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-            code,
-            grant_type: "authorization_code",
-          },
-        });
-        const { data } = response;
-
-        // 엑세스 토큰에서 사용자 ID 가져오기
-        const {
-          data: { sub, email }, // 변수 sub 및 email 추가
-        } = await axios.get<{
-          sub: string;
-          email: string; // email 속성 추가
-        }>("https://www.googleapis.com/oauth2/v3/userinfo", {
-          headers: {
-            Authorization: `Bearer ${data.access_token}`,
-          },
-        });
-
-        const googleUserResponse = {
-          id: sub, // 구글 사용자 ID
-          email, // 구글 사용자 이메일
-        };
-
         // 로그인 API 호출 및 AccessToken, UserId 전역 상태에 저장
         const result = await loginWithGoogle({
           variables: {
@@ -84,7 +89,7 @@ export default function CallbackGoogle() {
         router.push("/main");
       } catch (error) {
         console.error("로그인 예외:", error);
-        // setGoogleUserResponse(googleUserResponse);
+        setGoogleUserResponse(googleUserResponse as any);
         router.push("/nickname");
       }
     };
